092abedc63ca5fca0a43d21cbe3f8c079872ca83
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-07 23:59:09 -0400

    Added browser router to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles/index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import './styles/index.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
 
