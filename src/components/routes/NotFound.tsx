a6d583f88070db3b27253bebe8f7741014f64afb
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-09-16 13:31:58 -0400

    created not found route and stylesheet

---
+++ src/components/routes/NotFound.tsx
@@ -0,0 +1,11 @@
+import '../../styles/NotFound.css'
+
+const NotFound = () => {
+  return (
+    <div className='not-found'>
+      <p>404 Not Found</p>
+    </div>
+  )
+}
+
+export default NotFound
