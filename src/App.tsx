9ca8c04d3a91295a3f4122a9a133eddb29da33ca
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-09-16 13:33:21 -0400

    added not found route

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Home from './components/routes/Home'
+import NotFound from './components/routes/NotFound'
 
 const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound/>} />
       </Routes>
       <Footer />
     </div>
