cd2dbf8c2a952b8d3142ce7edaf0bb6a7edb6c9d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-09-18 22:41:43 -0400

    removed section

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -3,9 +3,7 @@ import '../../styles/Home.css'
 const Home = () => {
   return (
     <div className='home'>
-      <section className='intro'>
-        <h1>Intro</h1>
-      </section>
+      
     </div>
   )
 }
