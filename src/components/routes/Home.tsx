7d5de05a5f78a31fc0026e34dcf809d223d02c83
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-09-18 22:50:10 -0400

    added intro section

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -3,6 +3,9 @@ import '../../styles/Home.css'
 const Home = () => {
   return (
     <div className='home'>
+      <section className='intro'>
+        <p>Intro</p>
+      </section>
     </div>
   )
 }
