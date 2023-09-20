5c1714711a843a6838a3739726038df06c7a0ad5
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-09-19 23:53:23 -0400

    created portfolio files

---
+++ src/components/routes/Portfolio.tsx
@@ -0,0 +1,11 @@
+import '../../styles/Portfolio.css'
+
+const Portfolio = () => {
+  return (
+    <div className='portfolio'>
+      <p>Portfolio</p>
+    </div>
+  )
+}
+
+export default Portfolio
