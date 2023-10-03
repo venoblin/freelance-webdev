3b775597b8f04772af21c80ec5f5161ad8583b83
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-10-02 22:56:12 -0400

    added image styles

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -7,10 +7,7 @@ const Home = () => {
       <section className='intro'>
         <h1>Your <span>Website</span>, Our <span>Expertise</span></h1>
         <p className='sub-heading'>Elevate your online presence with an appealing website.</p>
-
-        <div className='image'>
         <img src={introImage} alt='Code editor' />
-        </div>
       </section>
 
       <section className='services'>
