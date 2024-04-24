f33ea422dcaeed0df798d54d4c6c31771b7f708d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-04-23 22:51:53 -0400

    worked on styling

--- src/components/routes/HomeRoute/HomeRoute.tsx
+++ src/components/routes/HomeRoute/HomeRoute.tsx
@@ -7,7 +7,10 @@ const HomeRoute = () => {
       <section className='intro'>
         <h1>Your <span>Website</span>, Our <span>Expertise</span></h1>
         <p className='sub-heading'>Elevate your online presence with an appealing website.</p>
-        <img src={introImage} alt='Code editor' />
+        
+        <div className='intro-img'>
+          <img src={introImage} alt='Code editor' />
+        </div>
       </section>
 
       <section className='services white'>
